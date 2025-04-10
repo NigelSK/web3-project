0b91e77b28ab32cdc926650f872c456a51f851f8
Mordecai Wapa <wapamordecai10000@gmail.com>
2025-04-11 00:10:01 +0100

    Deprecated Guide

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,11 +1,40 @@
+import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineClose } 'react-icons/ai'; 
+import { AiOutlineClose } from 'react-icons/ai'; 
 
-impot logo from '../../images'
+import logo from '../../images/logo.png';
+
+const NavbarItem = ({ title, classProps }) => {
+    return (
+
+    <li className={`mx-4 cursor-pointer ${classProps}`}>
+        {title}
+    </li>
+    );
+ }
 
 const Navbar = () => {
+    const [toggleMenu, setToggleMenu]  = useState(false);
     return (
-        <h1>Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className='md:flex-[0.5] flex-initial justify-center items-center'>
+<img src={logo} alt='logo' className='w-32 cursor-pointer'></img>
+            </div>
+            <ul className='text-white md:flex hidden list-none flex-row justify-between items-center flex-initial'>
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+                    <NavbarItem key={item + index} title={item} classProps={'text-white'} />
+                ))}
+                <li className='bg-[#2952e3] py-2 px-7 mx-4 text-center rounded-full cursor-pointer hover:bg-[#2546bd]'>
+                    Login
+                </li>
+                </ul> 
+                <div className='flex relative'>
+                    {toggleMenu
+                    ? <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
+                    : <HiMenuAlt4 fontsize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)} />
+                    }
+                </div>
+        </nav>
     );
 }
 
